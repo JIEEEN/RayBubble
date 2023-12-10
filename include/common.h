30350c03238f2093b5972e3bf97df024f75d3723
JIEEEN <tjtpp009@korea.ac.kr>
2023-12-10 19:40:28 +0900

    mod format, fix real2Index

--- include/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COMMON_H_
-#define __COMMON_H_
-
-#include <iostream>
-#include <vector>
-#include <tuple>
-#include <cstdlib>
-
-#endif // __COMMON_H_
--- include/namespace.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NAMESPACE_H_
-#define __NAMESPACE_H_
-
-#define RB_NAMESPACE RayBubble
-#define RB_NAMESPACE_BEGIN namespace RayBubble {
-#define RB_NAMESPACE_END }
-
-#endif // __NAMESPACE_H_
